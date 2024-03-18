68b63ae67fa04fcfa552894ac99d0a267de4c57a
Ricardo <ricardo@tresipunt.com>
2024-03-18 18:47:28 +0100

    Connexió a BD funcionant

--- DBConnection.h
+++ DBConnection.h
@@ -1,32 +1,16 @@
 #pragma once
 
 #include <iostream>
-
 using namespace MySql::Data::MySqlClient;
 using namespace std;
 
-namespace PetSalut {
+public ref class DBConnection
+{
+public:
+	DBConnection();
+	
+	MySqlConnection^ getConnection();
 
-	class DBConnection {
-	public:
-		// Constructor with connection details
-		DBConnection(const string& hostname, const string& port, const string& username,
-			const string& password, const string& database);
-
-		// Destructor for cleanup
-		~DBConnection();
-
-		void executeQuery(const string& query); // Placeholder for your implementation
-
-		// Execute an update or insert query (returns number of affected rows)
-		int executeUpdate(const string& query);
-
-
-		unique_ptr<MySqlConnection> getConnection();
-
-
-	private:
-		std::unique_ptr<MySqlConnection> connection;
-	};
-
-} // namespace MyProject
+private:
+	MySqlConnection^ conn;
+};
