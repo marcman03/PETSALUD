3c0e7ec34e18bd0d5af8e6645e9277663e06cf63
Ricardo <ricardo@tresipunt.com>
2024-03-18 18:58:21 +0100

    Formateo hecho

--- Form1.cpp
+++ Form1.cpp
@@ -1,11 +1,3 @@
 #include "pch.h"
-#include "Form1.h"
+#include "MyForm.h"
 
-inline System::Void CppCLRWinFormsProject::Form1::Form1_Load(System::Object^ sender, System::EventArgs^ e) {
-
-
-}
-inline System::Void CppCLRWinFormsProject::Form1::button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	System::Diagnostics::Debug::WriteLine("Button clicked");
-	clinica.selectAll();
-}
--- Form1.h
+++ Form1.h
@@ -1,10 +1,6 @@
-#pragma once
-
-#include "Clinica_Class.h"
+ï»¿#pragma once
 
 namespace CppCLRWinFormsProject {
-
-
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -13,8 +9,10 @@ namespace CppCLRWinFormsProject {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-
-
+	/// TEST GIT Nï¿½ 1
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
 	public ref class Form1 : public System::Windows::Forms::Form
 	{
 	public:
@@ -37,11 +35,6 @@ namespace CppCLRWinFormsProject {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ button1;
-	protected:
-	private: System::Windows::Forms::MenuStrip^ menuStrip1;
-	private: System::Windows::Forms::ToolStripMenuItem^ opció1ToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^ opció2ToolStripMenuItem;
 
 	protected:
 
@@ -58,70 +51,18 @@ namespace CppCLRWinFormsProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
-			this->opció1ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->opció2ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
-			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
-			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(12, 107);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
-			// 
-			// menuStrip1
-			// 
-			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
-				this->opció1ToolStripMenuItem,
-					this->opció2ToolStripMenuItem
-			});
-			this->menuStrip1->Location = System::Drawing::Point(0, 0);
-			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(284, 24);
-			this->menuStrip1->TabIndex = 1;
-			this->menuStrip1->Text = L"menuStrip1";
-			// 
-			// opció1ToolStripMenuItem
-			// 
-			this->opció1ToolStripMenuItem->Name = L"opció1ToolStripMenuItem";
-			this->opció1ToolStripMenuItem->Size = System::Drawing::Size(60, 20);
-			this->opció1ToolStripMenuItem->Text = L"Opció 1";
-			// 
-			// opció2ToolStripMenuItem
-			// 
-			this->opció2ToolStripMenuItem->Name = L"opció2ToolStripMenuItem";
-			this->opció2ToolStripMenuItem->Size = System::Drawing::Size(57, 20);
-			this->opció2ToolStripMenuItem->Text = L"Opció2";
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 261);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->menuStrip1);
-			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"Form1";
 			this->Text = L"Form1";
-			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
-			this->menuStrip1->ResumeLayout(false);
-			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
-
-	Clinica clinica;
-
-	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e);
-    private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
-
 	};
 }
