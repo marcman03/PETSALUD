7b6f3fa75b54f352ed3890dc813679b594ea87aa
Gambouu <marc.gamboa@estudiantat.upc.edu>
2024-03-20 12:13:37 +0100

    Pantalla Principal creada a Form1.h y pantalla de registrar usuari linkada

---
+++ RegUsu.h
@@ -0,0 +1,79 @@
+#pragma once
+
+namespace PetSalut {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de RegUsu
+	/// </summary>
+	public ref class RegUsu : public System::Windows::Forms::Form
+	{
+	public:
+		RegUsu(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~RegUsu()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(327, 170);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(545, 256);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// RegUsu
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->ClientSize = System::Drawing::Size(1184, 661);
+			this->Controls->Add(this->button1);
+			this->Name = L"RegUsu";
+			this->Text = L"Registrar Propietari";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
