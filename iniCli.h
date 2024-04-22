4623247de91dd293ca9e46ee507929981cfe4881
Ricardo <ricardo@tresipunt.com>
2024-04-22 19:50:52 +0200

    Afegit consulta y modificació

--- iniCli.h
+++ iniCli.h
@@ -36,13 +36,18 @@ namespace PetSalut {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Button^ submitButton;
+	private: System::Windows::Forms::Button^ SubmitButton;
+	private: System::Windows::Forms::Label^ UserLabel;
+	private: System::Windows::Forms::Label^ PasswordLabel;
 	protected:
-	private: System::Windows::Forms::Label^ userLabel;
-	private: System::Windows::Forms::Label^ passwordLabel;
-	private: System::Windows::Forms::TextBox^ passwordTextBox;
-	private: System::Windows::Forms::TextBox^ usernameTextBox;
-	private: System::Windows::Forms::Label^ clinicaLoginTitle;
+
+	protected:
+
+
+	private: System::Windows::Forms::TextBox^ PasswordTextBox;
+	private: System::Windows::Forms::TextBox^ UsernameTextBox;
+	private: System::Windows::Forms::Label^ ClinicaLoginTitle;
+
 
 	private:
 		/// <summary>
@@ -57,87 +62,89 @@ namespace PetSalut {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->submitButton = (gcnew System::Windows::Forms::Button());
-			this->userLabel = (gcnew System::Windows::Forms::Label());
-			this->passwordLabel = (gcnew System::Windows::Forms::Label());
-			this->passwordTextBox = (gcnew System::Windows::Forms::TextBox());
-			this->usernameTextBox = (gcnew System::Windows::Forms::TextBox());
-			this->clinicaLoginTitle = (gcnew System::Windows::Forms::Label());
+			this->SubmitButton = (gcnew System::Windows::Forms::Button());
+			this->UserLabel = (gcnew System::Windows::Forms::Label());
+			this->PasswordLabel = (gcnew System::Windows::Forms::Label());
+			this->PasswordTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->UsernameTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->ClinicaLoginTitle = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
-			// submitButton
+			// SubmitButton
 			// 
-			this->submitButton->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->SubmitButton->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->submitButton->Location = System::Drawing::Point(154, 284);
-			this->submitButton->Margin = System::Windows::Forms::Padding(2);
-			this->submitButton->Name = L"submitButton";
-			this->submitButton->Size = System::Drawing::Size(176, 61);
-			this->submitButton->TabIndex = 11;
-			this->submitButton->Text = L"Iniciar Sessió";
-			this->submitButton->UseVisualStyleBackColor = true;
-			this->submitButton->Click += gcnew System::EventHandler(this, &IniCli::submitButton_Click);
+			this->SubmitButton->Location = System::Drawing::Point(154, 284);
+			this->SubmitButton->Margin = System::Windows::Forms::Padding(2);
+			this->SubmitButton->Name = L"SubmitButton";
+			this->SubmitButton->Size = System::Drawing::Size(176, 61);
+			this->SubmitButton->TabIndex = 11;
+			this->SubmitButton->Text = L"Iniciar Sessió";
+			this->SubmitButton->UseVisualStyleBackColor = true;
+			this->SubmitButton->Click += gcnew System::EventHandler(this, &IniCli::SubmitButton_Click);
 			// 
-			// userLabel
+			// UserLabel
 			// 
-			this->userLabel->AutoSize = true;
-			this->userLabel->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->UserLabel->AutoSize = true;
+			this->UserLabel->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->userLabel->Location = System::Drawing::Point(144, 101);
-			this->userLabel->Name = L"userLabel";
-			this->userLabel->Size = System::Drawing::Size(212, 33);
-			this->userLabel->TabIndex = 10;
-			this->userLabel->Text = L"Nom d\'usuari";
+			this->UserLabel->Location = System::Drawing::Point(144, 101);
+			this->UserLabel->Name = L"UserLabel";
+			this->UserLabel->Size = System::Drawing::Size(212, 33);
+			this->UserLabel->TabIndex = 10;
+			this->UserLabel->Text = L"Nom d\'usuari";
 			// 
-			// passwordLabel
+			// PasswordLabel
 			// 
-			this->passwordLabel->AutoSize = true;
-			this->passwordLabel->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->PasswordLabel->AutoSize = true;
+			this->PasswordLabel->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->passwordLabel->Location = System::Drawing::Point(144, 192);
-			this->passwordLabel->Name = L"passwordLabel";
-			this->passwordLabel->Size = System::Drawing::Size(203, 33);
-			this->passwordLabel->TabIndex = 9;
-			this->passwordLabel->Text = L"Contrasenya";
+			this->PasswordLabel->Location = System::Drawing::Point(144, 192);
+			this->PasswordLabel->Name = L"PasswordLabel";
+			this->PasswordLabel->Size = System::Drawing::Size(203, 33);
+			this->PasswordLabel->TabIndex = 9;
+			this->PasswordLabel->Text = L"Contrasenya";
 			// 
-			// passwordTextBox
+			// PasswordTextBox
 			// 
-			this->passwordTextBox->Location = System::Drawing::Point(86, 233);
-			this->passwordTextBox->Margin = System::Windows::Forms::Padding(2);
-			this->passwordTextBox->Name = L"passwordTextBox";
-			this->passwordTextBox->Size = System::Drawing::Size(308, 20);
-			this->passwordTextBox->TabIndex = 8;
+			this->PasswordTextBox->Location = System::Drawing::Point(86, 233);
+			this->PasswordTextBox->Margin = System::Windows::Forms::Padding(2);
+			this->PasswordTextBox->Name = L"PasswordTextBox";
+			this->PasswordTextBox->Size = System::Drawing::Size(308, 20);
+			this->PasswordTextBox->TabIndex = 8;
+			this->PasswordTextBox->TextChanged += gcnew System::EventHandler(this, &IniCli::PasswordTextBox_TextChanged);
 			// 
-			// usernameTextBox
+			// UsernameTextBox
 			// 
-			this->usernameTextBox->Location = System::Drawing::Point(88, 143);
-			this->usernameTextBox->Margin = System::Windows::Forms::Padding(2);
-			this->usernameTextBox->Name = L"usernameTextBox";
-			this->usernameTextBox->Size = System::Drawing::Size(307, 20);
-			this->usernameTextBox->TabIndex = 7;
+			this->UsernameTextBox->Location = System::Drawing::Point(88, 143);
+			this->UsernameTextBox->Margin = System::Windows::Forms::Padding(2);
+			this->UsernameTextBox->Name = L"UsernameTextBox";
+			this->UsernameTextBox->Size = System::Drawing::Size(307, 20);
+			this->UsernameTextBox->TabIndex = 7;
+			this->UsernameTextBox->TextChanged += gcnew System::EventHandler(this, &IniCli::UsernameTextBox_TextChanged);
 			// 
-			// clinicaLoginTitle
+			// ClinicaLoginTitle
 			// 
-			this->clinicaLoginTitle->AutoSize = true;
-			this->clinicaLoginTitle->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 28.2F, System::Drawing::FontStyle::Regular,
+			this->ClinicaLoginTitle->AutoSize = true;
+			this->ClinicaLoginTitle->Font = (gcnew System::Drawing::Font(L"Gill Sans Ultra Bold", 28.2F, System::Drawing::FontStyle::Regular,
 				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
-			this->clinicaLoginTitle->Location = System::Drawing::Point(79, 29);
-			this->clinicaLoginTitle->Name = L"clinicaLoginTitle";
-			this->clinicaLoginTitle->Size = System::Drawing::Size(337, 54);
-			this->clinicaLoginTitle->TabIndex = 6;
-			this->clinicaLoginTitle->Text = L"Iniciar Sessió";
+			this->ClinicaLoginTitle->Location = System::Drawing::Point(79, 29);
+			this->ClinicaLoginTitle->Name = L"ClinicaLoginTitle";
+			this->ClinicaLoginTitle->Size = System::Drawing::Size(337, 54);
+			this->ClinicaLoginTitle->TabIndex = 6;
+			this->ClinicaLoginTitle->Text = L"Iniciar Sessió";
 			// 
 			// IniCli
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(490, 476);
-			this->Controls->Add(this->submitButton);
-			this->Controls->Add(this->userLabel);
-			this->Controls->Add(this->passwordLabel);
-			this->Controls->Add(this->passwordTextBox);
-			this->Controls->Add(this->usernameTextBox);
-			this->Controls->Add(this->clinicaLoginTitle);
+			this->ClientSize = System::Drawing::Size(492, 441);
+			this->Controls->Add(this->SubmitButton);
+			this->Controls->Add(this->UserLabel);
+			this->Controls->Add(this->PasswordLabel);
+			this->Controls->Add(this->PasswordTextBox);
+			this->Controls->Add(this->UsernameTextBox);
+			this->Controls->Add(this->ClinicaLoginTitle);
 			this->Name = L"IniCli";
 			this->Text = L"IniCli";
 			this->ResumeLayout(false);
@@ -145,12 +152,12 @@ namespace PetSalut {
 
 		}
 #pragma endregion
-	private: System::Void submitButton_Click(System::Object^ sender, System::EventArgs^ e) {
+	private: System::Void SubmitButton_Click(System::Object^ sender, System::EventArgs^ e) {
 
 		try {
 
-			String^ username = textBox1->Text;
-			String^ contrasenya = textBox2->Text;
+			String^ username = UsernameTextBox->Text;
+			String^ contrasenya = PasswordTextBox->Text;
 
 			TxIniciSessio iniS;
 
